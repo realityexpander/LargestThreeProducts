431855c9b0c5dfb52dce8eab97d0529f2eee39c3
Chris Athanas <realityexpanderdev@gmail.com>
2018-11-26 21:46:18 -0600

    Update largestProductOfThree.js

--- largestProductOfThree.js
+++ largestProductOfThree.js
@@ -10,10 +10,12 @@ function largestProductOfThree (array) {
 
   for(let i=array.length; i>=0; i--) {
     saveLargest(array[i]);
-//     console.log(array[i], largest)
+    console.log(array[i], largest)
   }
 
-  return largest[0] * largest[1] * largest[2];
+  let edgeCase = array[0] * array[1] * array[array.length - 1];
+
+  return Math.max(largest[0] * largest[1] * largest[2], edgeCase);
 
   function saveLargest(i) {
     for(let x=0; x<3; x++) {
@@ -26,6 +28,26 @@ function largestProductOfThree (array) {
 
 }
 
+    //       , /,
+    //     , /;/ |.-;
+    //   /|/ /.'/.-',_
+    // _,/  ._-    =-==.
+    // =\ =:        -|-.
+    // `\_-;  0 _0   |
+    // (        _)   |
+    // `\  _______  /
+    //   \ \     //'
+    //    \ \'--//
+    //     /`-`"`'\
+    //   |_|======|
+    //   |_|======|
+    //   ((/======|
+    //     #######_
+    //   .''-----" `'.
+    //   `-----------`
+
+
 // console.log(largestProductOfThree([ 2, 1, 3, 7 ])); // 42
 // console.log(largestProductOfThree([ 0, 2, 3 ])); // 0
-console.log(largestProductOfThree([-5,-1,-3,-2,-4])); // -6
+// console.log(largestProductOfThree([-5,-1,-3,-2,-4])); // -6
+console.log(largestProductOfThree([-31,41,34,-37,-17,29])); // 47027
