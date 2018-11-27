b01fac0aaca2e6d550c59164db8c9a2c2531573d
Chris Athanas <realityexpanderdev@gmail.com>
2018-11-26 19:07:38 -0600

    Create largestProductOfThree.js

---
+++ largestProductOfThree.js
@@ -0,0 +1,11 @@
+function largestProductOfThree (array) {
+
+}
+
+// Scan the array and compute Maximum, second maximum and 
+//   third maximum element present in the array.
+// Scan the array and compute Minimum and second minimum 
+//   element present in the array.
+// Return the maximum of product of Maximum, second maximum 
+//   and third maximum and product of Minimum, second minimum 
+//   and Maximum element.
